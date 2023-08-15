2f7c3ad399e4b7b1fa5aa914140bdd274c3ec146
Novarista13 <134473935+Novarista13@users.noreply.github.com>
2023-08-15 22:49:26 +0630

    set up basic structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,35 +1,79 @@
 import React from "react";
 import axios from "axios";
 import { useState } from "react";
+import "./App.css";
 import { ColorRing } from "react-loader-spinner";
 
-export default function Weather(props) {
+export default function Weather() {
   let [city, setCity] = useState("");
-  let [temp, setTemp] = useState(null);
+  let [response, setResponse] = useState({});
+  let [reload, setReload] = useState(false);
+
   function handleResponse(response) {
-    // alert(`The temperature in ${props.city} is ${response.data.main.temp}`);
-    setCity(props.city);
-    setTemp(Math.round(response.data.main.temp));
+    setReload(true);
+    setResponse({
+      city: response.data.name,
+      temp: Math.round(response.data.main.temp),
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      src: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
   }
 
-  let appId = "cd173a006b0e51dac58c6d8064c94178";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${appId}&units=metric`;
-  axios.get(url).then(handleResponse);
+  function submitHandler(event) {
+    event.preventDefault();
+    let appId = "cd173a006b0e51dac58c6d8064c94178";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}&units=metric`;
+    axios.get(url).then(handleResponse);
+    setCity("");
+  }
 
-  return (
-    <div>
-      <ColorRing
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="blocks-loading"
-        wrapperStyle={{}}
-        wrapperClass="blocks-wrapper"
-        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+  function showCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form className="input-group" onSubmit={submitHandler}>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="type a city"
+        value={city}
+        onChange={showCity}
       />
-      <h1>
-        The temperature in {city} is {temp}°C.
-      </h1>
-    </div>
+      <input type="submit" value="search" className="btn btn-primary" />
+    </form>
   );
+
+  if (reload) {
+    return (
+      <div className="Weather">
+        <h1>{form}</h1>
+        <div>City: {response.city}</div>
+        <div>Temp: {response.temp}°C</div>
+        <div>Description: {response.description}</div>
+        <div>Humidity: {response.humidity}%</div>
+        <div>Wind: {response.wind} km/h</div>
+        <div>
+          <img src={response.src} alt={response.description} />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Weather">
+        <h1>{form}</h1>
+        <ColorRing
+          visible={true}
+          height="30"
+          width="30"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+        />
+      </div>
+    );
+  }
 }
