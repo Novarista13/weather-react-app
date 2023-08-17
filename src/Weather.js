03f39b7b3b41051a7ef75a9deed6b91b51e73de3
Novarista13 <134473935+Novarista13@users.noreply.github.com>
2023-08-17 23:10:26 +0630

    solved the error and set up page flow

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Dashboard from "./Dashboard";
 import "./App.css";
 // import { ColorRing } from "react-loader-spinner";
 import "bootstrap/dist/css/bootstrap.css";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function Weather() {
   let [city, setCity] = useState("");
@@ -38,7 +38,7 @@ export default function Weather() {
     <form onSubmit={submitHandler}>
       <input
         type="text"
-        className="form-control"
+        className="form-control mb-2"
         placeholder="type a city"
         value={city}
         onChange={showCity}
@@ -47,17 +47,99 @@ export default function Weather() {
     </form>
   );
 
+  let Dashboard = (
+    <div className="row main-dashboard m-5">
+      <div className="col-6 col-md information-box row flex-column">
+        <div className="col row information flex-column">
+          <div className="day">Tuesday</div>
+          <div className="date">20 Jun 2023</div>
+          <div className="current-city">Biarritz, FR</div>
+        </div>
+        <div className="col row information flex-column">
+          <div className="mt-auto">
+            <div className="icon">
+              <ReactAnimatedWeather
+                icon="CLEAR_DAY"
+                color="white"
+                size={80}
+                animate={true}
+              />
+            </div>
+            <div className="temp">29 °C</div>
+            <div className="description">Sunny</div>
+          </div>
+        </div>
+      </div>
+      <div className="col-6 col-md p-0">
+        <div className=" weather-box">
+          <div className="row flex-column">
+            <div className="weather-condition-box">
+              <div className="weather-condition">
+                PRECIPITATION
+                <span className="float-end">{response.humidity}%</span>
+              </div>
+              <div className="weather-condition">
+                HUMIDITY
+                <span className="float-end">{response.humidity}% </span>
+              </div>
+              <div className="weather-condition">
+                WIND
+                <span className="float-end">{response.wind} km/h </span>
+              </div>
+            </div>
+            <div className="row forecast-box flex-row px-0 mx-0">
+              <div className="forecast col my-3 py-3 text-center">
+                <ReactAnimatedWeather
+                  icon="CLEAR_NIGHT"
+                  color="black"
+                  size={30}
+                  animate={true}
+                />
+                <div>Tue</div>
+                <div className="forecast-temp">29 °C</div>
+              </div>
+              <div className="forecast next-days  col my-3 py-3 text-center">
+                <ReactAnimatedWeather
+                  icon="PARTLY_CLOUDY_NIGHT"
+                  color="white"
+                  size={30}
+                  animate={true}
+                />
+                <div>Tue</div>
+                <div className="forecast-temp">29 °C</div>
+              </div>
+              <div className="forecast next-days  col my-3 py-3 text-center">
+                <ReactAnimatedWeather
+                  icon="SNOW"
+                  color="white"
+                  size={30}
+                  animate={true}
+                />
+                <div>Tue</div>
+                <div className="forecast-temp">29 °C</div>
+              </div>
+              <div className="forecast next-days col my-3 py-3 text-center">
+                <ReactAnimatedWeather
+                  icon="FOG"
+                  color="white"
+                  size={30}
+                  animate={true}
+                />
+                <div>Tue</div>
+                <div className="forecast-temp">29 °C</div>
+              </div>
+            </div>
+            {form}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   if (reload) {
     return (
       <div>
-        <Dashboard
-          city={response.city}
-          temp={response.city}
-          description={response.description}
-          humidity={response.humidity}
-          wind={response.wind}
-        />
-        {form}
+        {Dashboard}
         {/* <div>
           <img src={response.src} alt={response.description} />
         </div> */}
@@ -72,26 +154,6 @@ export default function Weather() {
       </div>
     );
   } else {
-    return (
-      <div>
-        <Dashboard
-          city="Yangon"
-          temp={0}
-          description="sunny"
-          humidity={0}
-          wind={0}
-        />
-        {form}
-        {/* <ColorRing
-          visible={true}
-          height="30"
-          width="30"
-          ariaLabel="blocks-loading"
-          wrapperStyle={{}}
-          wrapperClass="blocks-wrapper"
-          colors={["#5d8cc7", "#5d8cc7", "#5d8cc7", "#5d8cc7", "#5d8cc7"]}
-        /> */}
-      </div>
-    );
+    return <div>{Dashboard}</div>;
   }
 }
